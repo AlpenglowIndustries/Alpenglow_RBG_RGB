5e6bc385f12cbe237686f35c21aae15a5796fefb
Alpenglow Industries <42129728+AlpenglowInd@users.noreply.github.com>
2020-12-27 22:20:57 -0800

    now with PWM mirror on PB2

--- RBG_RGB.ino
+++ RBG_RGB.ino
@@ -1,11 +1,16 @@
 // Test Program for ATTiny4
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <stdint.h>
 
 #define MAXBRITE  75
 
 volatile uint32_t counter;
+
+ISR(TIM0_COMPA_vect) {
+  PINB |= (1 << PB2);
+}
 
 void delay (uint16_t time) {
   counter = 46 * time;
@@ -14,31 +19,39 @@ void delay (uint16_t time) {
 
 int main (void) {
   DDRB = (1 << DDB0 | 1 << DDB1 | 1 << DDB2);  // PB0, 1, 2 as outputs
-  OCR0A = 3;
+
+  TIMSK0 |= (1 << OCIE0A);  // enables output compare A match interrupt for PWM mirroring
+  sei();
+
+  OCR0A = 4;
   OCR0B = 255;
+  PORTB |= (1 << PB2);
   TCCR0A = (1 << COM0A1 | 1 << COM0B1 | 1 << WGM00);  // Phase-correct PWM 8-bit mode, A and B
   TCCR0B = (1 << CS01);  // clk/8, timer started
-
 
   while (1) {
 
     uint8_t i;
-    for (i = 3; i < MAXBRITE; i++){
+    for (i = 4; i < MAXBRITE; i++){
       OCR0A = i;
       OCR0B = MAXBRITE-i;
       delay(50);
     }
+
     OCR0A = MAXBRITE;
-    OCR0B = 0;  // special case, means output = 0
+    OCR0B = 4;
     delay(50);
 
-    for (i = MAXBRITE; i > 3; i--){
+
+    for (i = MAXBRITE; i > 4; i--){
       OCR0A = i;
       OCR0B = MAXBRITE-i;
       delay(50);
     }
-    OCR0A = 0;  // special case, means output = 0
+
+    OCR0A = 4;
     OCR0B = MAXBRITE;
     delay(50);
+
   }
 }
