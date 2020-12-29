820179525376986a7dd22531a6ffa08f6a63abd5
Alpenglow Industries <42129728+AlpenglowInd@users.noreply.github.com>
2020-12-28 19:43:07 -0800

    Working RGB fading!!!  Green is set for direct drive (backwards) because there is no inline transistor on proto board.

--- RBG_RGB.ino
+++ RBG_RGB.ino
@@ -8,7 +8,15 @@
 
 volatile uint32_t counter;
 
+// PB0 = red
+// PB1 = blue
+// PB2 = green
+
 ISR(TIM0_COMPA_vect) {
+  PINB |= (1 << PB2);   // toggles value of PB2, mirrors the OCR0A PB0 PWM
+}
+
+ISR(TIM0_COMPB_vect) {
   PINB |= (1 << PB2);
 }
 
@@ -17,41 +25,58 @@ void delay (uint16_t time) {
   do counter--; while (counter != 0);
 }
 
-int main (void) {
-  DDRB = (1 << DDB0 | 1 << DDB1 | 1 << DDB2);  // PB0, 1, 2 as outputs
+void cycle (void) {
+  uint8_t i;
+  for (i = 4; i < MAXBRITE; i++){
+    OCR0A = MAXBRITE-i;     // red
+    OCR0B = i;              // blue
+    delay(50);
+  }
+}
 
-  TIMSK0 |= (1 << OCIE0A);  // enables output compare A match interrupt for PWM mirroring
+int main (void) {
+
+  DDRB = (1 << DDB0 | 1 << DDB1 | 1 << DDB2);  // PB0, 1, 2 as outputs
   sei();
 
-  OCR0A = 4;
-  OCR0B = 255;
-  PORTB |= (1 << PB2);
   TCCR0A = (1 << COM0A1 | 1 << COM0B1 | 1 << WGM00);  // Phase-correct PWM 8-bit mode, A and B
   TCCR0B = (1 << CS01);  // clk/8, timer started
 
   while (1) {
 
-    uint8_t i;
-    for (i = 4; i < MAXBRITE; i++){
-      OCR0A = i;
-      OCR0B = MAXBRITE-i;
+    PORTB |= (1 << PB2);  // turns green off (direct drive)
+    uint8_t j;
+    for (j = 4; j < MAXBRITE; j++){
+      OCR0A = j;
+      OCR0B = MAXBRITE-j;
       delay(50);
     }
 
-    OCR0A = MAXBRITE;
-    OCR0B = 4;
-    delay(50);
+    DDRB &= ~(1 << DDB1);       // turns blue off
+    while (!(TIFR0 &= (1 << TOV0)));
+    PORTB &= ~(1 << PB2);
+    TIMSK0 |= (1 << OCIE0B);    // enables output compare B match interrupt for green PWM mirroring on red
+    TIFR0 |= (1 << TOV0);
+    cycle();
+    while (!(TIFR0 &= (1 << TOV0)));
+    TIMSK0 &= ~(1 << OCIE0B);   // disables PWM mirroring
+    TIFR0 |= (1 << TOV0);
+    OCR0B = 4;                  // sets blue to low
+    DDRB |= (1 << DDB1);        // turns blue output on
+//    PORTB |= (1 << PB2);        // sets green to full on
+    PORTB &= ~(1 << PB2);
 
+    DDRB &= ~(1 << DDB0);       // turns red off
+    while (!(TIFR0 &= (1 << TOV0)));
+    TIMSK0 |= (1 << OCIE0A);    // enables output compare A match interrupt for green PWM mirroring on blue
+    TIFR0 |= (1 << TOV0);
+    cycle();
+    while (!(TIFR0 &= (1 << TOV0)));
+    TIMSK0 &= ~(1 << OCIE0A);   // disables PWM mirroring
+    TIFR0 |= (1 << TOV0);
+//    PORTB &= ~(1 << PB2);       // sets green to low
+    OCR0A = 4;                  // sets red to low
+    DDRB |= (1 << DDB0);        // turns red output on
 
-    for (i = MAXBRITE; i > 4; i--){
-      OCR0A = i;
-      OCR0B = MAXBRITE-i;
-      delay(50);
-    }
-
-    OCR0A = 4;
-    OCR0B = MAXBRITE;
-    delay(50);
-
-  }
-}
+  }   // end while(1)
+}     // end main
